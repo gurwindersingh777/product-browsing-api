cc717ef5bf9ff9df5755218f7695bd2d8468434b
Gurwinder Singh <singhgurwinder2004@gmail.com>
2026-06-24 12:26:36 +0530

    feat: add product API tester HTML interface and update server configuration

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import 'dotenv/config';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import productRouter from './routes/product.route.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use("/products", productRouter)
 
